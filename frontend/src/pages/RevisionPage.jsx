d83b887dc8f2721ee1d85fe9aa4dde01f71c367b
ishitamangla <ishitamangla10@gmail.com>
2026-09-20 20:43:39 +0530

    updated vite_ in front of env variable

--- frontend/src/pages/RevisionPage.jsx
+++ frontend/src/pages/RevisionPage.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import QuestionCard from "../components/QuestionCard";
 import AnswerBox from "../components/AnswerBox";
 
-const API_URL = import.meta.env.API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 function RevisionPage() {
   const location = useLocation();
   const navigate = useNavigate();
